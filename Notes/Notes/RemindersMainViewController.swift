162447726cae2979c3f89d666100ff46adaad5e8
FelixXiao <felixxiao0805@gmail.com>
2017-01-12 17:31:26 +0800

    first commit

---
+++ Notes/Notes/RemindersMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RemindersMainViewController.swift
+//  Notes
+//
+//  Created by FelixXiao on 2017/1/12.
+//  Copyright © 2017年 FelixXiao. All rights reserved.
+//
+
+import UIKit
+
+class RemindersMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
