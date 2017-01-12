1c9813ce3374cec94b5efe401ea6fc894c37c135
Apple <apple@114-212-134-208.nju.edu.cn>
2017-01-12 18:28:47 +0800

    aaa

--- Notes/Notes/NotesMainViewController.swift
+++ Notes/Notes/NotesMainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class NotesMainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    
+    ...
     override func viewDidLoad() {
         super.viewDidLoad()
 
