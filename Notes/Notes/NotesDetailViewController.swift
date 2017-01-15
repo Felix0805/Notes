e460a43b4b8cbc079c148e7945183678a7b8f6c1
FelixXiao <felixxiao0805@gmail.com>
2017-01-15 11:30:55 +0800

    UI_complete

--- Notes/Notes/NotesDetailViewController.swift
+++ Notes/Notes/NotesDetailViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class NotesDetailViewController: UIViewController {
 
+    @IBOutlet weak var abc: NSLayoutConstraint!
     
     @IBOutlet weak var Save: UIBarButtonItem!
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextView: UITextView!
     
+    var deltaY : CGFloat = 0
+    var Y: CGFloat = 0
     var notesIndex = 0
     var isAdd = 0
     var titleString = "title"
@@ -29,6 +32,25 @@ class NotesDetailViewController: UIViewController {
         titleTextField.text = titleString
         contentTextView.text = contentString
         self.automaticallyAdjustsScrollViewInsets = false
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        let swipe = UISwipeGestureRecognizer(target:self, action:#selector(swipe(_:)))
+        swipe.direction = .left
+        self.view.addGestureRecognizer(swipe)
+        
+        let swipe2 = UISwipeGestureRecognizer(target:self, action:#selector(swipe2(_:)))
+        swipe2.direction = .right
+        self.view.addGestureRecognizer(swipe2)
+        
+        let swipe3 = UISwipeGestureRecognizer(target:self, action:#selector(swipe3(_:)))
+        swipe3.direction = .up
+        self.view.addGestureRecognizer(swipe3)
+        
+        let swipe4 = UISwipeGestureRecognizer(target:self, action:#selector(swipe4(_:)))
+        swipe4.direction = .down
+        self.view.addGestureRecognizer(swipe4)
 
     }
 
@@ -51,11 +73,48 @@ class NotesDetailViewController: UIViewController {
 
     }
 
-
+    
+    func swipe(_ recognizer:UISwipeGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    func swipe2(_ recognizer:UISwipeGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    func swipe3(_ recognizer:UISwipeGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    func swipe4(_ recognizer:UISwipeGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    func keyBoardWillShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect {
+            // 计算可能需要上移的距离
+            let keyboardHeight = keyboardFrame.origin.y
+            let Height = contentTextView.frame.maxY
+            deltaY = keyboardHeight - Height
+            Y += deltaY
+        }
+        //    self.view.bringSubview(toFront: NavigationBar)
+        abc.constant -= deltaY
+        UIView.animate(withDuration: 0.5, animations: {() -> Void in
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func keyBoardWillHide(notification: NSNotification) {
+        abc.constant += Y
+        Y = 0
+    }
     
     
     /*
-    // MARK: - Navigation
+    // MARK: -
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
